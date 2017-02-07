4d2a771ca603f58198c7c0d0d304472eb0bfcbd4
Konrad Gladysz <gladysz.konrad@gmail.com>
2017-02-07 12:49:33 +0000

    Dedacted sensitive inforation

--- mailer.php
+++ mailer.php
@@ -3,14 +3,14 @@ require 'phpmail/PHPMailerAutoload.php';
 
 $mail = new PHPMailer;
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'lh41.dnsireland.com';  // Specify main and backup SMTP servers
+$mail->Host = '*';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'project-control@konrad.ie';                 // SMTP username
-$mail->Password = '[U$@16gFJ)[_';                           // SMTP password
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Username = '*';                 // SMTP username
+$mail->Password = '*';                           // SMTP password
+$mail->SMTPSecure = '*';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
 $mail->setFrom($_POST['email'], $_POST['name']);
-$mail->addAddress('project-control@konrad.ie');
+$mail->addAddress('*');
 $mail->addReplyTo($_POST['email'], $_POST['name']);
 $mail->isHTML(true);                                  // Set email format to HTML
 $mail->Subject = $_POST['subject'];
